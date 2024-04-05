641f39ac70cab73991172d8eb287c8fbab46620a
mobyitalong <165924835+Mobyitalong@users.noreply.github.com>
2024-04-05 21:28:01 +0000

    write functions `read_csv()` and `parse_data()`

--- src/app/exercise_1/quiz.go
+++ src/app/exercise_1/quiz.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+type quizQuestion struct {
+	question string
+	answer   string
+}
+
+func check_for_errors(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func parse_data(data [][]string) []quizQuestion {
+	var questions []quizQuestion
+
+	for _, row := range data {
+		questions = append(
+			questions,
+			quizQuestion{question: row[0], answer: row[1]},
+		)
+	}
+
+	return questions
+}
+
+func read_csv(file_path string) {
+	file, err := os.Open(file_path)
+
+	if err != nil {
+		check_for_errors(err)
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 2
+
+	data, err := reader.ReadAll()
+	if err != nil {
+		check_for_errors(err)
+	}
+
+}
+
+func main() {
+	read_csv("/src/app/exercise_1/problems.csv")
+}
